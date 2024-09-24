522d7bcb601b618e9417272197dd9f8ca24581d9
FourMinato <kropgerken@gmail.com>
2024-09-24 15:47:14 +0700

    ordersCommit

--- api/orders/orders.ts
+++ api/orders/orders.ts
@@ -13,7 +13,7 @@ export const router = express.Router();
 //สร้างรายการส่งสินค้า
 
 
-router.post('/create-delivery-order/:user_id', upload.single('itemImage'), (req, res) => {
+router.post('/create-order/:user_id', upload.single('itemImage'), (req, res) => {
     const { user_id } = req.params;
     const { itemName, itemDescription, receiverPhone } = req.body;
     const itemImage = req.file ? req.file.path : null;
