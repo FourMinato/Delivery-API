8a0add9c4ac4918fe60d09f1405a6b65247bda20
FourMinato <kropgerken@gmail.com>
2024-09-24 16:14:30 +0700

    MyOrderCommit

--- api/orders/orders.ts
+++ api/orders/orders.ts
@@ -10,6 +10,38 @@ const upload = multer({ dest: 'uploads/' });
 export const router = express.Router();
 
 
+router.get("/", (req, res) => {
+
+    const sql = "select * from delivery_orders";
+
+    conn.query(sql, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({
+                message: 'Internal Server Error'
+            });
+        } else {
+            if (result.length > 0) {
+
+                res.status(200).json({
+                    success: true,
+                    message: 'Get Data Success',
+                    data: result
+
+                });
+
+            } else {
+                res.status(400).json({
+                    message: 'Get Data failed'
+                });
+            }
+        }
+    });
+
+});
+
+
+
 //สร้างรายการส่งสินค้า
 
 
