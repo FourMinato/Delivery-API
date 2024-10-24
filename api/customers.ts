36ee453b907398c29f99b5086913c8bb2eaf5d4a
FourMinato <kropgerken@gmail.com>
2024-10-24 23:09:01 +0700

    Profile

--- api/customers.ts
+++ api/customers.ts
@@ -164,7 +164,7 @@ router.post("/register", upload.single('profile_image'), async (req, res) => {
 
         if (req.file) {
             const dateTime = giveCurrrentDateTime();
-            const storageRef = ref(storage, `files/${req.file.originalname + "_" + dateTime}`);
+            const storageRef = ref(storage, `Profile_Rider/${req.file.originalname + "_" + dateTime}`);
 
             const metadata = {
                 contentType: req.file.mimetype,
@@ -249,7 +249,7 @@ router.post("/register/riders", upload.single('profile_image'), async (req, res)
 
         if (req.file) {
             const dateTime = giveCurrrentDateTime();
-            const storageRef = ref(storage, `files/${req.file.originalname + "_" + dateTime}`);
+            const storageRef = ref(storage, `Profile_Rider/${req.file.originalname + "_" + dateTime}`);
 
             const metadata = {
                 contentType: req.file.mimetype,
